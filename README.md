436acaf5ed436b09ebd2c2b4be2fd3baf8d49580
Andrea Pugnana <andrepugni@gmail.com>
2024-10-04 15:35:09 +0200

    AISTATS 2025 updates

--- README.md
+++ README.md
@@ -27,16 +27,25 @@ python train.py -data all --defer_system all --seed 42
 This will train the models for all the datasets and all the deferring systems. 
 The results will be saved in the `resultsRAW` folder.
 
-To obtain the final estimates included in our paper, you can run the following command:
+To obtain the final estimates for Q1 and Q3 included in our paper, you can run the following command:
 
 ```
 python test.py
-python test_conditional.py
 ```
 
 The first command will produce the final estimates for all the datasets and all the deferring systems for Q1 and Q3.
+
+To obtain the final estimates for Q2 included in our paper, you can run the following commands:
+
+```
+python demographic.py
+python test_conditional.py
+```
+
+The first command adds the demographic information to the `xray-airspace` dataset.
 The second command will produce the final estimates for all the datasets and all the deferring systems for Q2.
 The results will be saved in the `results` folder.
 
-
 If you do not want to train the models from scratch, you can download them from [here](https://www.dropbox.com/scl/fo/6rxx0sy4dq1c86aqgw5sr/AIdZeeWh6yeSohg4oLjiJKY?rlkey=1in2itm23tx1nh4jaaht4hxwm&dl=0).
+
+All the plots and the tables can be retrieved from running the Jupyter notebooks in the `notebooks` folder.
---
+++ demographic.py
@@ -0,0 +1,37 @@
+
+from src.datasets import ChestXrayDataset
+from src.utils import set_seed
+from torch import optim
+import pandas as pd
+import numpy as np
+
+def main():
+    """
+    The script adds the demographic info to the test results of the ChestXray dataset.
+    """
+    seed = 42
+    label_chosen = 2
+    set_seed(seed)
+    # load the dataset
+    dataset = ChestXrayDataset(False, True, data_dir="data", label_chosen=label_chosen, batch_size=128,
+                               test_split=.2, val_split=.10)
+    # load the demographic info
+    db = pd.read_csv("data/Data_Entry_2017_v2020.csv")
+    # merge the demographic info with the test results
+    test_data = pd.DataFrame(dataset.data_test_loader.dataset.images, columns=["Index"])
+    test_data["Image Index"] = test_data["Index"].str.split("/", expand=True)[2].copy()
+    df = test_data.merge(db, on="Image Index")
+    # save the results
+    for method in ["ASM", "SP", "RS", "CC", "OVA", "DT", "LCE"]:
+        raws = pd.read_csv("resultsRAW/chestxray2/GCresultsRAW_test_chestxray2_{}_42_ep3.csv".format(method))
+
+        tmp = pd.concat([df, raws], axis=1)
+        cols = ["Image Index", "Patient ID", "Patient Age", "Patient Gender", 'rej_score', 'labels',
+                'hum_preds', 'preds', 'class_probs_0', 'class_probs_1']
+        tmp = tmp[cols].copy()
+        tmp.to_csv(
+            "resultsRAW/chestxray2/{}_Scenario1_Conditional_test_chestxray{}_42_ep3.csv".format(method, label_chosen),
+            index=False)
+
+if __name__ == "__main__":
+    main()
